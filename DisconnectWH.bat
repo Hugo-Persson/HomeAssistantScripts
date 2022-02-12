1e81a750579ab5fdb4ea5697b54bd9f3c7d7c744
Hugo Persson <hugo.e.persson@gmail.com>
2022-02-12 19:02:28 +0100

    Simple script to connect and disconnect bluetooth

---
+++ DisconnectWH.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Bluetooth Command Line Tools\bin\btcom" -n "WH-H900N (h.ear)" -r -s111e
+"C:\Program Files (x86)\Bluetooth Command Line Tools\bin\btcom" -n "WH-H900N (h.ear)" -r -s110b
